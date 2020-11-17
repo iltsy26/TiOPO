3463e04ddf758ece92b8bcac22ed77f5424d295a
iltsy26 <71667539+iltsy26@users.noreply.github.com>
2020-11-17 09:40:46 +0300

    Add files via upload

---
+++ Lab7_TiOPO/Lab7_TiOPO/Acceleration.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Lab7_TiOPO
+{
+    class Acceleration
+    {
+        private double StartSpeed, accel;
+        private int time;
+
+        private Acceleration() { }
+
+        public Acceleration(double Nspeed, double Naccel, int Ntime )
+        {
+            StartSpeed = Nspeed; accel = Naccel; time = Ntime;
+        }
+
+        private void Load()
+        {
+            StartSpeed = Convert.ToDouble(Console.ReadLine());
+            accel = Convert.ToDouble(Console.ReadLine());
+            time = Convert.ToInt32(Console.ReadLine());
+        }
+
+        public static Acceleration CreateAccelFromFile()
+        {
+            double Spd = Convert.ToDouble(Console.ReadLine());
+            double Accl = Convert.ToDouble(Console.ReadLine());
+            int Time = Convert.ToInt32(Console.ReadLine());
+            return new Acceleration (Spd, Accl, Time);
+        }
+
+        public double Distance()
+        {
+            return StartSpeed * time + (accel * Math.Pow(time, 2)) / 2;
+        }
+
+        public double CurrentSpeed()
+        {
+            return StartSpeed + accel * time;
+        }
+
+        public void Info()
+        {
+            String str = "\n--------------------------\n" +
+                         "\n Равноускоренное движение\n" +
+                        "\n--------------------------\n";
+
+            Console.WriteLine(str);
+            Console.WriteLine(String.Format("Initial Speed = {0:0.00}", StartSpeed));
+            Console.WriteLine(String.Format("Acceleration = {0:0.00}", accel));
+            Console.WriteLine(String.Format("Time = {0:0.00}", time));
+            Console.Write(String.Format("Distance({0:0.00}", time));
+            Console.WriteLine(String.Format(") = {0:0.00}", Distance()));
+            Console.Write(String.Format("Speed({0:0.00}", time));
+            Console.WriteLine(String.Format(") = {0:0.00}", CurrentSpeed()));
+
+        }
+        public void Info(ConsoleColor fg, ConsoleColor bgc)
+        {
+            Console.ForegroundColor = fg;
+            Console.BackgroundColor = bgc;
+            Console.Clear();
+            Info();
+        }
+    }
+}
---
+++ Lab7_TiOPO/Lab7_TiOPO/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Lab7_TiOPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if !DEBUG
+            TextWriter save_out = Console.Out;
+            TextReader save_in = Console.In;
+            var new_out = new StreamWriter(@"accel_output.txt");
+            var new_in = new StreamReader(@"accel_input.txt");
+            Console.SetOut(new_out);
+            Console.SetIn(new_in);
+#endif
+            Acceleration A1, A2;
+
+#if DEBUG
+            A2 = new Acceleration(4.5, 4, 2);
+            A2.Info(ConsoleColor.Yellow, ConsoleColor.Blue);
+#endif
+
+#if !DEBUG  
+            A1 = Acceleration.CreateAccelFromFile();
+            A1.Info();
+#endif
+
+#if !DEBUG
+           Console.SetOut(save_out); new_out.Close();
+           Console.SetIn(save_in); new_in.Close();
+#else
+            Console.ReadKey();
+#endif
+        }
+    }
+}
