0ae983e074e091381330d9fabf4b4dbd20a5caaf
iltsy26 <71667539+iltsy26@users.noreply.github.com>
2020-12-29 00:19:15 +0300

    Add files via upload

---
+++ Lab10_TiOPO/Lab10_TiOPO/MatrixOperations.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_TiOPO
+{
+    class MatrixOperations
+    {
+        public static double[,] Multiplication(double[,] matrixA, double[,] matrixB)
+        {
+            if (matrixA.GetLength(1) != matrixB.GetLength(0))
+            {
+                throw new Exception("Умножение не возможно! Количество столбцов первой матрицы не равно количеству строк второй матрицы.");
+            }
+
+            double[,] matrixC = new double[matrixA.GetLength(0), matrixB.GetLength(1)];
+
+            for (var i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrixB.GetLength(1); j++)
+                {
+                    matrixC[i, j] = 0;
+
+                    for (var k = 0; k < matrixA.GetLength(1) ; k++)
+                    {
+                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                    }
+                }
+            }
+            return matrixC;
+        }
+    }
+}
---
+++ Lab10_TiOPO/Lab10_TiOPO/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10_TiOPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Clear();
+
+            Matrix m = new Matrix();
+
+            double[,] M1 = new double[5, 5];
+
+            if (m.LoadMatrix(M1, "Matrix1.txt"))
+                m.PrintMatrix(M1);
+
+            Console.WriteLine();
+
+            System.Threading.Thread.Sleep(1000);
+
+
+            double[,] M2 = new double[5, 5];
+
+            if (m.LoadMatrix(M2, "Matrix2.txt"))
+                m.PrintMatrix(M2);
+
+            Console.WriteLine();
+
+            double[,] ResMatrix = new double[5, 5];
+            ResMatrix = MatrixOperations.Multiplication(M1, M2);
+            m.PrintMatrix(ResMatrix);
+
+            Console.ReadKey();
+        }
+    }
+}
